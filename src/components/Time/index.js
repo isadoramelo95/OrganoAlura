de29d4a7244ebb5371e10d2d35b4b35a730666e3
isado <isado@LAPTOP-UC5IQEQ6>
2024-03-14 23:31:15 -0300

    React até aula 4

---
+++ src/components/Time/index.js
@@ -0,0 +1,13 @@
+import './styles.css'
+
+const Time = (props) => {
+    const css = { backgroundColor: props.corSecundaria }
+    
+    return (
+        <section className='time' style={css }>
+           <h3>style={{borderColor:  props.corPrimaria}} {props.nome}</h3>
+        </section>
+    )
+}
+
+export default Time
